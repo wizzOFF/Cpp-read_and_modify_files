d627c41065967847c58582c7058e82de70c8e969
WizzOFF <maher_off@proton.me>
2025-03-31 11:37:48 +0200

    read_and_modify_files.cpp: open a file for writing

---
+++ read_and_modify_files.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+   ofstream monFlux("README");
+   //Déclaration d'un flux permettant d'écrire dans le fichier
+   // README
+   return 0;
+}
